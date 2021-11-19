7f7317fb66ac0207e47599d447677109399bc166
RomanovYO <94523600+RomanovYO@users.noreply.github.com>
2021-11-19 18:40:36 +0200

    Изменение mean,sigma (персонализация)

--- chart2.py
+++ chart2.py
@@ -22,11 +22,11 @@ def plot_chart2():
 
   # Данные для графика
   mean1  = 0
-  mean2  = 1
+  mean2  = 2
   mean3  = -2
   sigma1 = 1
   sigma2 = 2
-  sigma3 = 0.7
+  sigma3 = 0.5
   x = np.arange(-7, 7, 0.01)
   f1 = np.exp(-np.square((x-mean1)/sigma1)/2)/(np.sqrt(2*np.pi)*sigma1)
   f2 = np.exp(-np.square((x-mean2)/sigma2)/2)/(np.sqrt(2*np.pi)*sigma2)
