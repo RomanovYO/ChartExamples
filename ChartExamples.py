5c499906bea3d42fdb7e6011e1d6bf2232391346
RomanovYO <94523600+RomanovYO@users.noreply.github.com>
2021-11-19 12:36:29 +0200

    Добавление метки заголовка

--- ChartExamples.py
+++ ChartExamples.py
@@ -11,6 +11,10 @@ window.title('Примеры построения графиков')
 def do_close():
   window.destroy()
 
+# Добавление метки заголовка
+lblTitle = tk.Label(text = 'Примеры построения графиков', font = ('Helvetica', 16, 'bold'), fg = '#0000cc')
+lblTitle.place(x = 55, y = 25)
+
 # Добавление кнопки закрытия программы
 btnClose = tk.Button(window, text = 'Закрыть', font = ('Helvetica', 10, 'bold'), command = do_close)
 btnClose.place(x = 330, y = 400, width = 90, height = 30)
